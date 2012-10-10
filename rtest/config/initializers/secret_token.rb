430112f06a18e758287051d968b1239bc090a0ef
root <root@ip-10-248-31-63.eu-west-1.compute.internal>
2012-10-10 18:12:07 +0000

    init

---
+++ rtest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rtest::Application.config.secret_token = '8d76c7fbfca73c5456cc1f68b1041d9e59a785deb1e5d488b7cfb1c1cd37f69f0b61174808c0618d835b27dc333c10c0562ad3c0273c6d2af45627a84b2c10ae'
